023c2d2594c4d06527ac712943f115e2eb23e623
Alvaro Gonzalez Andrades <92127698+alvaroga2003@users.noreply.github.com>
2025-04-03 10:57:28 +0200

    cambio22

---
+++ src/app/alvaro/alvaro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alvaro',
+  imports: [],
+  templateUrl: './alvaro.component.html',
+  styleUrl: './alvaro.component.css'
+})
+export class AlvaroComponent {
+
+}
