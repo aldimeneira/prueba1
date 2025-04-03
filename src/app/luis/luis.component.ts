29d02b0f9282c1bad62521025effa06a85384325
ivanpaul03 <133638592+ivanpaul03@users.noreply.github.com>
2025-04-03 11:24:48 +0200

    cambio

---
+++ src/app/luis/luis.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-luis',
+  imports: [],
+  templateUrl: './luis.component.html',
+  styleUrl: './luis.component.css'
+})
+export class LuisComponent {
+
+}
