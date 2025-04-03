e07a3b0d68c4db77c8bf20cfbebea7992a479c82
Jose <you@example.com>
2025-04-03 10:15:19 +0200

    cambio

---
+++ src/app/ivan/ivan.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ivan',
+  imports: [],
+  templateUrl: './ivan.component.html',
+  styleUrl: './ivan.component.css'
+})
+export class IvanComponent {
+
+}
